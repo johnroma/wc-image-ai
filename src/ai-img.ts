7ffe1967944784551041020c97d2ac16e9ea41fd
johnroma <login@john.ro>
2024-01-07 21:10:30 +0300

    cleanup and remove settimout

--- src/ai-img.ts
+++ src/ai-img.ts
@@ -1,7 +1,8 @@
 import { LitElement, html, css } from "lit"
-import { customElement, eventOptions, property } from "lit/decorators.js"
+import { customElement, property } from "lit/decorators.js"
 import { spread } from "@open-wc/lit-helpers"
 import { getGeneratedImage } from "./get-generated-image"
+
 @customElement("ai-img")
 export class AiImg extends LitElement {
   @property({ type: String }) fallback = ""
@@ -9,15 +10,38 @@ export class AiImg extends LitElement {
   @property({ type: String, reflect: true }) height = ""
   @property({ type: String, reflect: true }) src = ""
   @property({ type: String, reflect: true }) alt = ""
-  @property({ type: String, reflect: false })
-  imgsrc = ""
-  @property({ type: String, reflect: true })
-  prompt = ""
+  @property({ type: String, reflect: false }) imgsrc = ""
+  @property({ type: String, reflect: true }) prompt = ""
 
   imgAttributes: { [key: string]: string } = {}
 
   connectedCallback() {
     super.connectedCallback()
+    this.initAttributes()
+    this.fetchImage()
+  }
+
+  async fetchImage() {
+    if (!this.prompt) {
+      this.imgsrc = this.fallback
+      return
+    }
+
+    try {
+      const openAiResponse = await getGeneratedImage(
+        this.src,
+        this.prompt,
+        Number(this.width),
+        Number(this.height)
+      )
+      this.imgsrc = openAiResponse || this.fallback
+    } catch (error) {
+      console.error("Error fetching AI image:", error)
+      this.imgsrc = this.fallback
+    }
+  }
+
+  initAttributes() {
     Array.from(this.attributes).forEach((attr) => {
       if (
         ![
@@ -49,46 +73,6 @@ export class AiImg extends LitElement {
       )
   }
 
-  // Override createRenderRoot to render in the light DOM
-  // createRenderRoot() {
-  //   return this
-  // }
-
-  protected constructor() {
-    super()
-
-    setTimeout(async () => {
-      if (!this.prompt) {
-        this.imgsrc = this.fallback
-        return
-      }
-
-      try {
-        const openAiResponse = await getGeneratedImage(
-          this.src,
-          this.prompt,
-          Number(this.width),
-          Number(this.height)
-        )
-        this.imgsrc = openAiResponse || this.fallback
-      } catch (error) {
-        console.error("Error fetching AI image:", error)
-        this.imgsrc = this.fallback
-      }
-
-      const img = this.shadowRoot?.querySelector("img")
-
-      //add event listeners
-      //img?.addEventListener("load", this._myFunction)
-      img?.addEventListener("error", this._onImgError.bind(this))
-    }, 1)
-  }
-
-  @eventOptions({ passive: true })
-  private _onImgError() {
-    this.imgsrc = this.fallback
-  }
-
   protected render() {
     const hostStyles = {
       width: `${this.width}px`,
@@ -110,18 +94,6 @@ export class AiImg extends LitElement {
               decoding="async"
               loading="eager"
               ${spread(this.imgAttributes)}
-              @onerror=${() => {
-                console.log("onerror")
-              }}
-              @onabort=${() => {
-                console.log("onabort")
-              }}
-              @onloadend=${() => {
-                console.log("onloadend")
-              }}
-              @onprogress=${() => {
-                console.log("onprogress")
-              }}
             />
           `
         : html`<div>fail</div>`}
